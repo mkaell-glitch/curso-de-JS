54bc3d9943065f45f136dd4b28f3457f27178824
mkaell-glitch <leafzyx@gmail.com>
2026-09-26 12:12:27 -0300

    function e suas condiçoes :)

---
+++ aula11/ex008.js
@@ -0,0 +1,9 @@
+var vel = 59
+console.log(`a velocidade do seu carro é de ${vel}KM/H`)
+if (vel > 60) { //condiçao simples
+    console.log('limites de KM/H ultrapassado... multado!')
+}
+else {
+    console.log('dentro dos limites de segurança, boa viagem :D')
+}
+console.log(`dirija sempre usando cinto de velocidade!`)
---
+++ aula11/ex009.js
@@ -0,0 +1,9 @@
+var pais = 'frança'
+console.log(`vivendo em ${pais}`)
+if (pais == 'brasil') {
+    console.log('voce é brasileiro!')
+}
+
+else {
+    console.log('voce é estrangeiro!')
+}
